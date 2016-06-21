af00d33475a62ff3b5961c14f7f0dd29f8aa58c9
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-06-21 14:07:56 -0600

    n

--- init.js
+++ init.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
+    function gameSetUp() {
+        var textArray = ["penguin1", "penguin2", "penguin3", "penguin4", "penguin5", "penguin6", "penguin7", "penguin8", "yeti"];
 
-    var textArray = ["penguin1", "penguin2", "penguin3", "penguin4", "penguin5", "penguin6", "penguin7", "penguin8", "yeti"];
+        function addMounds() {
+            var randomNumber = Math.floor(Math.random() * textArray.length);
+            var div = document.createElement('div');
+            div.className = textArray[randomNumber];
+            $('#items').append(div);
+            textArray.splice(randomNumber, 1);
+        }
 
-    function addMounds() {
-        var randomNumber = Math.floor(Math.random() * textArray.length);
-        var div = document.createElement('div');
-        div.className = textArray[randomNumber];
-        $('#items').append(div);
-        textArray.splice(randomNumber, 1);
-    }
-
-    for (var i = 0; i < textArray.length; i++) {
-        addMounds();
-        i--;
+        $('#items').empty();
+        for (var i = 0; i < textArray.length; i++) {
+            addMounds();
+            i--;
+        }
     }
     //This code will run after your page loads
-    $(".yeti").mousedown(function() {
-        document.getElementById('growl').play()
-        // alert("Oh no!");
+    $("body").on('click', '.yeti', function() {
+        document.getElementById('growl').play();
+        gameSetUp();
     })
+    gameSetUp();
 });
